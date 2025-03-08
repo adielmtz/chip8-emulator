4acd8d2bb8ace03f57beba843f4422301c5b1800
Luis Adiel Martínez <luisadiel99@gmail.com>
2025-03-08 10:08:53 -0600

    Implement 0xxx instructions

--- src/chip8.c
+++ src/chip8.c
@@ -9,6 +9,15 @@
 #define MEM_PC_START 0x200
 #define MEM_SP_START 0x1FE
 
+/* Stack & register operations */
+#define STACK_PUSH()    do { chip->sp -= 2; } while (0)
+#define STACK_POP()     do { chip->sp += 2; } while (0)
+#define JUMP_ADDR(addr) do { chip->pc = (((addr) & 0x0FFF) - 2); } while (0)
+#define NEXT_OPCODE()   do { chip->pc += 2; } while (0)
+
+/* VM handlers */
+#define OPCODE_HANDLER(op) static inline void VM_OpCode_Handler_##op(Chip8 *chip, const u16 opcode)
+
 /* Shorthands */
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -33,6 +42,34 @@ const static u8 FONT_LIST[] = {
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
+/**
+ * 0nnn: Jump to native assembler subroutine at 0xNNN (unused opcode).
+ */
+OPCODE_HANDLER(dw)
+{
+    chip->state |= ST_ERROR;
+}
+
+/**
+ * 00E0: Clear the screen.
+ */
+OPCODE_HANDLER(cls)
+{
+    memset(chip->dp, 0, sizeof(chip->dp));
+    chip->state |= ST_REDRAW;
+}
+
+/**
+ * 00EE: Return from subroutine to address pulled from stack.
+ */
+OPCODE_HANDLER(ret)
+{
+    STACK_POP();
+    u8 hi = chip->mem[chip->sp];
+    u8 lo = chip->mem[chip->sp + 1];
+    JUMP_ADDR((hi << 8) | lo);
+}
+
 void chip8_init(Chip8 *chip)
 {
     memset(chip, 0, sizeof(Chip8));
@@ -41,4 +78,15 @@ void chip8_init(Chip8 *chip)
     chip->pc = MEM_PC_START;
     chip->sp = MEM_SP_START;
     chip->state = ST_RUNNING;
+}
+
+void chip8_step(Chip8 *chip)
+{
+    u8 hi = chip->mem[chip->pc];
+    u8 lo = chip->mem[chip->pc + 1];
+    u16 opcode = (hi << 8) | lo;
+
+    /* Switch machine goes here */
+
+    NEXT_OPCODE();
 }
