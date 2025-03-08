a6831fac22adbc1d1c274e48aa11066a2d0999b1
Luis Adiel Martínez <luisadiel99@gmail.com>
2025-03-08 09:43:53 -0600

    Add CHIP8 struct

---
+++ src/chip8.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdint.h>
+
+#define DISPLAY_WIDTH  64
+#define DISPLAY_HEIGHT 32
+
+#define ST_RUNNING (1 << 0)
+#define ST_PAUSED  (1 << 1)
+#define ST_REDRAW  (1 << 2)
+#define ST_ERROR   (1 << 3)
+
+typedef struct Chip8
+{
+    uint8_t mem[4096];
+    uint8_t dp[DISPLAY_WIDTH * DISPLAY_HEIGHT];
+    uint8_t V[16];
+
+    // Keyboard
+    uint8_t kb[16];
+
+    // Pointers
+    uint16_t pc;
+    uint16_t sp;
+    uint16_t I;
+
+    // Extra
+    uint16_t state;
+
+    // Timers
+    uint8_t delay;
+    uint8_t sound;
+} Chip8;
