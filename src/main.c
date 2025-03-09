1f41f0d5584e1936f82b1c6ba732454b2d5a157d
Luis Adiel Martínez <luisadiel99@gmail.com>
2025-03-08 18:19:06 -0600

    Add SDL3 configuration to CMake

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
+#include <SDL3/SDL.h>
 
 int main(void)
 {
-    printf("Hello world!\n");
+    if (!SDL_Init(SDL_INIT_VIDEO /*| SDL_INIT_AUDIO*/ | SDL_INIT_EVENTS)) {
+        return; /* TODO: Return an ERROR here */
+    }
+
+    SDL_Window *window = SDL_CreateWindow("CHIP-8", 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_HIGH_PIXEL_DENSITY);
+    if (!window) {
+        return; /* TODO: Return an ERROR here */
+    }
+
+    SDL_Delay(5000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
